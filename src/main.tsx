09fa50f52c75278fed084e94e2d459af6cf569a9
luis <luisitohblas@gmail.com>
2024-11-22 10:12:11 +0100

    chakra importato

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 //import { Provider } from "@/components/ui/provider"
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider,createSystem,defineConfig } from '@chakra-ui/react'
 
+const config=defineConfig({
+  theme:{
+
+  },
+})
+
+const system=createSystem(config);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ChakraProvider value={system}>
     <App />
+    </ChakraProvider>
   </StrictMode>,
 )
--- src/modules/module.ts
+++ src/modules/module.ts
@@ -1,3 +1,4 @@
+
 export const OPTS = {
     method: "GET",
     headers: {
